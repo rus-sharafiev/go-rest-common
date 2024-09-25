b92307128c27884bd5a00461c4b21039a72000d2
Rus Sharafiev <srr@live.ru>
2024-09-25 17:22:48 +0300

    Fix serving dir

--- spa/handler.go
+++ spa/handler.go
@@ -7,51 +7,50 @@ import (
 	"strings"
 
 	common "github.com/rus-sharafiev/go-rest-common"
-	"github.com/rus-sharafiev/go-rest-common/exception"
 )
 
 type handler struct{}
 
-// ServeHTTP inspects the URL path to locate a file within the static dir
-// on the SPA handler. If a file is found, it will be served. If not, the
-// file located at the index path on the SPA handler will be served. This
-// is suitable behavior for serving an SPA (single page application).
 func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	path, err := filepath.Abs(r.URL.Path)
-	if err != nil {
-		exception.BadRequestError(w, err)
-		return
-	}
-
-	path = filepath.Join(*common.Config.StaticDir, path)
+	path := filepath.Join(*common.Config.StaticDir, r.URL.Path)
 	index := "index.html"
+	fileType := filepath.Ext(path)
 
-	_, err = os.Stat(path)
-	if os.IsNotExist(err) {
-		http.ServeFile(w, r, filepath.Join(*common.Config.StaticDir, index))
+	acceptGzip := strings.Contains(r.Header.Get("Accept-Encoding"), "gzip")
+
+	// Check whether a file exists or is a directory
+	if fi, err := os.Stat(path); os.IsNotExist(err) || fi.IsDir() {
+		htmlFile := filepath.Join(*common.Config.StaticDir, index)
+		if acceptGzip {
+			if _, err := os.Stat(htmlFile + ".gz"); err == nil {
+				w.Header().Add("Content-Encoding", "gzip")
+				w.Header().Add("Content-Type", "text/html")
+				htmlFile += ".gz"
+			}
+		}
+
+		// Serve SPA
+		http.ServeFile(w, r, htmlFile)
 		return
 	} else if err != nil {
-		exception.InternalServerError(w, err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
 	// Serve gziped
-	if strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
+	if acceptGzip && (fileType == ".js" || fileType == ".css") {
+		if _, err := os.Stat(path + ".gz"); err == nil {
 
-		if fileType := filepath.Ext(path); fileType == ".js" || fileType == ".css" {
-			if _, err := os.Stat(path + ".gz"); err == nil {
-
-				w.Header().Add("Content-Encoding", "gzip")
-				if fileType == ".js" {
-					w.Header().Add("Content-Type", "application/javascript")
-				}
-				if fileType == ".css" {
-					w.Header().Add("Content-Type", "text/css")
-				}
-				http.ServeFile(w, r, filepath.Join(path+".gz"))
-
-				return
+			w.Header().Add("Content-Encoding", "gzip")
+			if fileType == ".js" {
+				w.Header().Add("Content-Type", "text/javascript")
 			}
+			if fileType == ".css" {
+				w.Header().Add("Content-Type", "text/css")
+			}
+
+			http.ServeFile(w, r, filepath.Join(path+".gz"))
+			return
 		}
 	}
 
