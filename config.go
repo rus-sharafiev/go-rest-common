9e2ece092b5f9525423efa5341f462ed7ce631b1
Rus Sharafiev <srr@live.ru>
2024-11-19 09:41:23 +0300

    Add telegram key and auth token

--- config.go
+++ config.go
@@ -1,6 +1,9 @@
 package common
 
 import (
+	"encoding/json"
+	"fmt"
+	"os"
 	"slices"
 )
 
@@ -19,6 +22,8 @@ type config struct {
 	Port              *string          `json:"port"`
 	StaticDir         *string          `json:"staticDir"`
 	JwtKey            *string          `json:"jwtKey"`
+	TelegramKey       *string          `json:"telegramKey"`
+	TelegramAuthToken *string          `json:"telegramAuthToken"`
 	RecaptchaSecret   *string          `json:"recaptchaSecret"`
 	MailLogin         *string          `json:"mailLogin"`
 	MailPassword      *string          `json:"mailPassword"`
@@ -29,11 +34,35 @@ type config struct {
 
 var Config config
 
+func LoadConf() {
+	const confFile = "./config.json"
+
+	if _, err := os.Stat(confFile); err != nil && os.IsNotExist(err) {
+		return
+	} else if err != nil {
+		fmt.Printf("\nConfig file: \x1b[31m%v: %v\x1b[0m", "Error opening the file", err)
+		return
+	}
+
+	data, err := os.ReadFile(confFile)
+	if err != nil {
+		fmt.Printf("\nConfig file: \x1b[31m%v: %v\x1b[0m", "Error reading the file", err)
+		return
+	}
+
+	var config config
+	if err = json.Unmarshal(data, &config); err != nil {
+		fmt.Printf("\nConfig file: \x1b[31m%v: %v\x1b[0m", "Unmarshalling error", err)
+		return
+	}
+
+	Config = config
+}
+
 func (c config) IsNotValid() bool {
 	mandatotyFields := []*string{
 		c.Db,
 		c.Port,
-		c.StaticDir,
 		c.JwtKey,
 	}
 	return slices.Contains(mandatotyFields, nil)
